8bb8c750bd21f570856face0efe7150d63bfe6f4
stephengallo1977 <stephengallo1977@gmail.com>
2019-08-30 16:48:41 -0400

    addedScripts

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({
+    extended: true
+}))
+app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"))
+}
+
+app.use((req, res) =>
+    res.sendFile(path.join(__dirname, "client/public/index.html")))
+
+app.listen(PORT, () => console.log("Listening on PORT: " + PORT))
